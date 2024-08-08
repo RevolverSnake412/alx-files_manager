73ce8d2700193ab9ee74773eccc882827eac67e4
RevolverSnake412 <oussag@hotmail.fr>
2024-08-08 20:59:56 +0100

    Create a new user

---
+++ controllers/UserController.js
@@ -0,0 +1,37 @@
+const crypto = require('crypto');
+const dbClient = require('../utils/db');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const userExists = await dbClient.db.collection('users').findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+    const result = await dbClient.db.collection('users').insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    const newUser = {
+      id: result.insertedId,
+      email: email,
+    };
+
+    return res.status(201).json(newUser);
+  }
+}
+
+module.exports = UsersController;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const routes = require('./routes/index');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json()); // To parse JSON bodies
 app.use('/', routes);
 
 app.listen(port, () => {
